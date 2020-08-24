8f9449249323269292b2ad2f9bd07071632af63d
AShaver43 <gofrodude_197@hotmail.com>
2020-08-24 01:45:27 -0700

    completed Search page

---
+++ src/pages/MemePages/MemeSearch.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import MemeImg from '../../components/MemeImg'
+import MemeModel from '../../model/MemesModel'
+
+class MemeSearch extends Component {
+    state = {
+        memes: []
+    }
+
+    componentDidMount() {
+        this.fetchData()
+    }
+
+    fetchData = () => {
+        MemeModel.search().then(data => this.setState({ memes: data.data }))
+    }
+    render() {
+        let memeList = this.state.memes.map((meme, i) => {
+            return <Link to={`/memes/${ meme.id }`} key={i}>
+                <MemeImg url={ meme.url } />
+            </Link>
+        })
+        return (
+            <>
+                {this.state.memes ? memeList : 'Loading.....'}
+            </>
+        )
+    }
+}
+
+export default MemeSearch

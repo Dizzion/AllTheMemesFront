66c655327c8e8ba8b8d6e86514f64cd3e2def278
AShaver43 <gofrodude_197@hotmail.com>
2020-08-24 01:44:14 -0700

    added scss file for app styling and added header as component along with routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
+import './App.scss'
+import Header from './components/Header'
+import Routes from './routes/Routes'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header />
+        { Routes }
       </div>
     )
   }

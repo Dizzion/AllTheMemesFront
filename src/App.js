861958e7ea76fbc3d8e23f1d32a6063008dc636f
AShaver43 <gofrodude_197@hotmail.com>
2020-08-25 20:54:10 -0700

    refactored app.js and auth to work inside of session storage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.scss'
 import Header from './components/Header'
 import Routes from './routes/Routes'
+import { withRouter } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        { Routes }
-      </div>
-    )
+function App(props) {
+
+  const [user, setUser] = useSessionStorage("user", null)
+
+  const storeUser = (user) => {
+    setUser(user)
   }
+
+  const logout = (e) => {
+    e.preventDefault()
+    sessionStorage.clear()
+    setUser(null)
+    props.history.push('/memes')
+  }
+
+  return (
+    <div className="App">
+      <Header user={user} logout={logout} />
+      <Routes
+        user={user}
+        storeUser={storeUser}
+      />
+    </div>
+  )
 }
 
-export default App
+export default withRouter(App)
+
+function useSessionStorage(key, initialValue) {
+
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = sessionStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      console.log(error)
+      return initialValue
+    }
+  })
+
+  const setValue = value => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value
+      setStoredValue(valueToStore)
+      sessionStorage.setItem(key, JSON.stringify(valueToStore))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return [storedValue, setValue]
+}
--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -1,21 +1,13 @@
 import axios from 'axios'
 
-const url = `https://localhost:8080/api/auth`
+const url = `http://localhost:8080/api/auth`
 
 class AuthService {
     static login(username, password) {
         return axios.post(`${url}/login`, {username, password})
-            .then(res => {
-                if(res.data.accessToken) {
-                    sessionStorage.setItem("user", JSON.stringify(res.data))
-                }
-                return res.data
-            })
+            
     }
 
-    static logout() {
-        sessionStorage.removeItem("user")
-    }
 
     static signup(username, email, password) {
         return axios.post(`${url}/signup`, {
@@ -24,7 +16,7 @@ class AuthService {
     }
 
     static getCurrentUser() {
-        return JSON.parse(sessionStorage.getItem("user"))
+        return JSON.parse(localStorage.getItem("user"))
     }
 }
 

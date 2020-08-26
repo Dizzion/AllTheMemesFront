7fd4cfe47590382b146840bb4d8df5d20c3234c9
AShaver43 <gofrodude_197@hotmail.com>
2020-08-25 20:54:50 -0700

    redid routes to take props down from app

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import Home from '../pages/Home'
 import MemeSearch from '../pages/MemePages/MemeSearch'
@@ -11,13 +11,19 @@ import Login from '../pages/AuthPages/Login'
 import Signup from '../pages/AuthPages/Signup'
 import Profile from '../pages/AuthPages/Profile'
 
-export default (
+export default (props) => (
     <Switch>
         <Route exact path='/memes' component={ Home } />
-        <Route path='/login' component={ Login } />
         <Route path='/signup' component={ Signup } />
         <Route path='/profile' component={ Profile } />
-        <Route path='/memes/search/:hashtag' component={ MemeSearch } />
+        <Route path='/login' render={ (routeComponentProps) => {
+            return <Login
+                        {...routeComponentProps}
+                        user={props.user}
+                        storeUser={props.storeUser}
+                    />
+        } } /> 
+        <Route exact path='/memes/search/:hashtag' component={ MemeSearch } />
         <Route path='/memes/new' component={ NewMeme } />
         <Route exact path='/memes/:id' component={ MemeShow } />
         <Route path='/memes/:id/edit' component={ EditMeme } />

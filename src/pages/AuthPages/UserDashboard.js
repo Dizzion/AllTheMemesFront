3cb537b3f1ecdfc1be81bb6711c6b02f997148f1
AShaver43 <gofrodude_197@hotmail.com>
2020-08-25 23:28:35 -0700

    created UserDashboard

---
+++ src/pages/AuthPages/UserDashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UserDashboard extends Component {
+    render() {
+        return (
+            <div>
+                <h2>All Users</h2>
+            </div>
+        )
+    }
+}

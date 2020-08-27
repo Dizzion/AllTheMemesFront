f5bde2bcccbc2d8528dd09d7feb1b93c4139cf84
AShaver43 <gofrodude_197@hotmail.com>
2020-08-27 15:37:41 -0700

    made profile page return user info

--- src/pages/AuthPages/Profile.js
+++ src/pages/AuthPages/Profile.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import AuthService from '../../service/AuthService'
 
 export default class Profile extends Component {
     render() {
+        const user = AuthService.getCurrentUser()
+        console.log(user)
         return (
-            <div>
-                <h4>Profile</h4>
+            <div className="container">
+                <h1 className="h3 mt-5 mb-3 font-weight-normal">Profile</h1>
+                <p>Username: {user.username}</p>
+                <p>Email: {user.email}</p>
+                {user.roles}
             </div>
         )
     }

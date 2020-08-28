625580d43c093b8134f7e3cc8442b6cbe4133cec
AShaver43 <gofrodude_197@hotmail.com>
2020-08-27 17:08:28 -0700

    added cards for comments

--- src/components/ShowInfo/Comment.js
+++ src/components/ShowInfo/Comment.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react'
 class Comment extends Component {
     render() {
         return (
-            <>
-                <p>{this.props.body}</p>
-                <h5>{this.props.userPosted}</h5>
-            </>
+            <div className="card" id="comments">
+                <div className="card-body">
+                    <h5 className="card-title">{this.props.userPosted}</h5>
+                    <p className="card-text">{this.props.body}</p>
+                </div>
+            </div>
         )
     }
 }

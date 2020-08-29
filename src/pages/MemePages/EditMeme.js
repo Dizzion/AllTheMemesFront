1bc3d59ff6d4a617ebcc9a87dd76e2af111046ee
AShaver43 <gofrodude_197@hotmail.com>
2020-08-29 01:25:16 -0700

    styling fixes from deployment and hidding stuff that broke on deployment

--- src/pages/MemePages/EditMeme.js
+++ src/pages/MemePages/EditMeme.js
@@ -61,13 +61,13 @@ export default class EditMeme extends Component {
         }
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault()
         let hashTags = []
-        this.state.hashTags.forEach((tag) => {
+        await this.state.hashTags.forEach((tag) => {
             if (tag !== '') hashTags.push(tag)
         })
-        this.setState({hashTags})
+        await this.setState({ hashTags })
         MemeModel.update(this.state)
             .then(data => this.props.history.push(`/memes/${this.props.match.params.id}`))
     }
@@ -75,55 +75,55 @@ export default class EditMeme extends Component {
     render() {
         return (
             <div className="container">
-                <img src={this.state.url ? this.state.url : 'Loading.....'}  alt=""/>
+                <img src={this.state.url ? this.state.url : 'Loading.....'} alt="" />
                 <h1 className="h3 mt-5 mb-3 font-weight-normal">Edit the Hashtags of this Meme</h1>
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-inline">
-                    <div className="form-group">
-                        <label htmlFor="tag1">#</label>
-                        <input
-                            type="text"
-                            name="tag1"
-                            className="form-control mr-2"
-                            onChange={this.handleChange}
-                            value={this.state.hashTags ? this.state.hashTags[0] : 'Loading.....'} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="tag2">#</label>
-                        <input
-                            type="text"
-                            name="tag2"
-                            className="form-control mr-2"
-                            onChange={this.handleChange}
-                            value={this.state.hashTags ? this.state.hashTags[1] : 'Loading.....'} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="tag3">#</label>
-                        <input
-                            type="text"
-                            name="tag3"
-                            className="form-control mr-2"
-                            onChange={this.handleChange}
-                            value={this.state.hashTags ? this.state.hashTags[2] : 'Loading.....'} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="tag4">#</label>
-                        <input
-                            type="text"
-                            name="tag4"
-                            className="form-control mr-2"
-                            onChange={this.handleChange}
-                            value={this.state.hashTags ? this.state.hashTags[3] : 'Loading.....'} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="tag5">#</label>
-                        <input
-                            type="text"
-                            name="tag5"
-                            className="form-control"
-                            onChange={this.handleChange}
-                            value={this.state.hashTags ? this.state.hashTags[4] : 'Loading.....'} />
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="tag1">#</label>
+                            <input
+                                type="text"
+                                name="tag1"
+                                className="form-control mr-2"
+                                onChange={this.handleChange}
+                                value={this.state.hashTags ? this.state.hashTags[0] : 'Loading.....'} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="tag2">#</label>
+                            <input
+                                type="text"
+                                name="tag2"
+                                className="form-control mr-2"
+                                onChange={this.handleChange}
+                                value={this.state.hashTags ? this.state.hashTags[1] : 'Loading.....'} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="tag3">#</label>
+                            <input
+                                type="text"
+                                name="tag3"
+                                className="form-control mr-2"
+                                onChange={this.handleChange}
+                                value={this.state.hashTags ? this.state.hashTags[2] : 'Loading.....'} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="tag4">#</label>
+                            <input
+                                type="text"
+                                name="tag4"
+                                className="form-control mr-2"
+                                onChange={this.handleChange}
+                                value={this.state.hashTags ? this.state.hashTags[3] : 'Loading.....'} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="tag5">#</label>
+                            <input
+                                type="text"
+                                name="tag5"
+                                className="form-control"
+                                onChange={this.handleChange}
+                                value={this.state.hashTags ? this.state.hashTags[4] : 'Loading.....'} />
+                        </div>
                     </div>
                     <button className="btn btn-lg btn-secondary mt-4" type="submit">Post It!</button>
                 </form>
